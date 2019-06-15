f89ddeb4dc3817ec5b28901b9e01c386fb89941e
JackDing <361164217@qq.com>
2019-06-15 23:22:52 +0800

    重构FM

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <Player></Player>
+  </div>
+</template>
+
+<script>
+import Player from './components/Player'
+import './assets/common.css'
+import './assets/style.css'
+
+
+export default {
+  name: 'App',
+  components: {
+    Player
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
