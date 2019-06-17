8e66e1fa84bb219aa5f1591cedcdf9274aae585a
JackDing <361164217@qq.com>
2019-06-17 23:48:35 +0800

    基本功能重构

--- src/components/Player.vue
+++ src/components/Player.vue
@@ -8,18 +8,20 @@
             <h2>周杰伦</h2>
             <figure ref="figure"></figure>
             <div class="action">
-              <svg class="icon" aria-hidden="true" v-if="!isRecycle">
+              <svg class="icon" aria-hidden="true" v-if="!isRecycle"
+                   @click="recyclePlay">
                 <use xlink:href="#icon-xunhuanbofang"></use>
               </svg>
-              <svg class="icon " aria-hidden="true" v-if="isRecycle">
+              <svg class="icon " aria-hidden="true" v-if="isRecycle"
+                   @click="normalPlay">
                 <use xlink:href="#icon-xunhuanjiagongcishu"></use>
               </svg>
               <svg class="icon " aria-hidden="true" v-if="!isPlaying"
-              @click="playSong">
+                   @click="playSong">
                 <use xlink:href="#icon-bofang"></use>
               </svg>
               <svg class="icon " aria-hidden="true" v-if="isPlaying"
-              @click="pauseSong"
+                   @click="pauseSong"
               >
                 <use xlink:href="#icon-zantingtingzhi"></use>
               </svg>
@@ -29,7 +31,7 @@
             </div>
           </div>
           <div class="right">
-            <div class="tagName">{{currentChannnelName}}</div>
+            <div class="tagName">{{(currentChannnelName)?currentChannnelName:'类型'}}</div>
             <h1>{{(currentSong)?currentSong.title:'七里香'}}</h1>
             <h2>{{(currentSong)?currentSong.artist:'周杰伦'}}</h2>
             <div class="lyrics-wrapper">
@@ -55,9 +57,10 @@
 
             <div class="progress">
               <div class="progressBar">
-                <div class="currentBar"></div>
+                <div class="currentBar" ref="progress"></div>
+                <div class="circle" ref="circle"></div>
               </div>
-              <div class="duration">-0:00</div>
+              <div class="duration">-{{currentTime}}</div>
             </div>
           </div>
         </div>
@@ -69,10 +72,10 @@
         <div class="channel-wrapper">
           <ul class="channels " ref="channels">
             <li class="channel" v-for="value in channelList" :key="value.channel_id"
-            @click="selectChannel(value.channel_id,value.name)"
+                @click="selectChannel(value.channel_id,value.name)"
             >
               <img :src="value.cover_small" alt="">
-              <h3 class="title">{{value.name}}</h3>
+              <h3 class="title">-{{value.name}}</h3>
             </li>
           </ul>
         </div>
@@ -91,81 +94,152 @@
 <script>
   import '../assets/svg'
   import axios from 'axios'
+
   export default {
     name: "Player",
-    data(){
-      return{
-        channelList:null,
-        currentChannnel:'',
-        currentChannnelName:'',
-        currentSong:null,
-        audio:new Audio(),
-        isPlaying:false,
-        isRecycle:false,
-        initScroll:0
+    data() {
+      return {
+        channelList: null,
+        currentChannnel: '',
+        currentChannnelName: '',
+        currentSong: null,
+        audio: new Audio(),
+        isPlaying: false,
+        isRecycle: false,
+        initScroll: 0,
+        currentLyrics: null,
+        duration: 0,
+        timer: null,
       }
     },
     created() {
       axios.get('//jirenguapi.applinzi.com/fm/getChannels.php')
-        .then((res)=>{
-          this.channelList=res.data.channels
+        .then((res) => {
+          this.channelList = res.data.channels
           console.log(res.data.channels);
         })
     },
-    computed:{
-      listWidth(){
-        let {width}= this.$refs.channels.getBoundingClientRect()
-        return width
-      }
-    },
-    methods:{
-      selectChannel(id,name){
-        console.log(id);
-        this.currentChannnel=id
-        this.currentChannnelName=name
+    mounted() {
 
+      this.listenAudio()
+
+    },
+    computed: {
+      listWidth() {
+        let {width} = this.$refs.channels.getBoundingClientRect()
+        return width
+      },
+      currentTime() {
+        if (this.duration) {
+          let minutes = parseInt(this.duration / 60)
+          minutes = minutes >= 10 ? '' + minutes : '0' + minutes
+          let seconds = parseInt(this.duration % 60)
+          seconds = seconds >= 10 ? '' + seconds : '0' + seconds
+          return minutes + ':' + seconds
+        }
+
+      }
+
+    },
+    methods: {
+      selectChannel(id, name) {
+        this.currentChannnel = id
+        this.currentChannnelName = name
         axios.get(`//jirenguapi.applinzi.com/fm/getSong.php?channel=${id}`)
-          .then((res)=>{
-            this.currentSong=res.data.song[0]
-            console.log(this.currentSong);
+          .then((res) => {
+            this.currentSong = res.data.song[0]
             this.setCurrentSong()
+            this.getLyrics()
+
           })
       },
-      setCurrentSong(){
-        this.audio.src=this.currentSong.url
-        this.$refs.figure.style.backgroundImage=`url(${this.currentSong.picture})`
+      setCurrentSong() {
+        this.audio.src = this.currentSong.url
+        this.$refs.figure.style.backgroundImage = `url(${this.currentSong.picture})`
+
+        this.audio.oncanplaythrough = () => {
+          clearInterval(this.timer)
+          this.timer = null
+          this.audio.play()
+          this.isPlaying = true
+          this.duration = this.audio.duration
+        }
       },
-      nextSong(){
+      nextSong() {
         axios.get(`//jirenguapi.applinzi.com/fm/getSong.php?channel=${this.currentChannnel}`)
-          .then((res)=>{
-            this.currentSong=res.data.song[0]
-            console.log(this.currentSong);
+          .then((res) => {
+            this.currentSong = res.data.song[0]
             this.setCurrentSong()
+            this.getLyrics()
+
           })
       },
-      playSong(){
-        this.isPlaying=true
-        this.audio.play()
-        this.audio.autoplay=true
+      getLyrics() {
+        axios.get('//jirenguapi.applinzi.com/fm/getLyric.php', {
+          params: {
+            sid: this.currentSong.sid
+          }
+        }).then((res) => {
+          console.log(res.data);
+        })
       },
-      pauseSong(){
-        this.isPlaying=false
+      playSong() {
+        this.isPlaying = true
+        this.audio.play()
+      },
+      pauseSong() {
+        this.isPlaying = false
         this.audio.pause()
       },
-      leftList(){
-        this.initScroll-=1
-        if(this.initScroll<0){this.initScroll=0}
-        this.$refs.channels.style.transform=`translateX(-${50 * this.initScroll}%)`
+      recyclePlay() {
+        this.isRecycle = true
+        this.audio.loop = true
       },
-      rightList(){
-        this.initScroll+=1
-        if(this.initScroll>19){this.initScroll=19}
-        this.$refs.channels.style.transform=`translateX(-${50 * this.initScroll}%)`
+      normalPlay() {
+        this.isRecycle = false
+        this.audio.loop = false
+      },
+      listenAudio() {
+        this.audio.addEventListener('pause', () => {
+          if (this.audio.ended === true && this.isRecycle === false) {
+            console.log('自动切歌');
+            this.nextSong()
+          }
+          clearInterval(this.timer)
+          this.timer = null
+        })
+        this.audio.addEventListener('play', () => {
+          this.timer = setInterval(() => {
+            this.showProgress()
+            this.duration -= 0.5
+          }, 500)
+        })
+      },
+      showProgress(){
+        let progress=`${100*this.audio.currentTime/this.audio.duration}%`
+        this.$refs.progress.style.width=progress
+        let {width}=this.$refs.progress.getBoundingClientRect()
+        this.$refs.circle.style.transform=`translate(calc(-50% + ${width}px),calc(-50% - 4px))`
+        console.log(progress);
+      },
+      leftList() {
+        this.initScroll -= 1
+        if (this.initScroll < 0) {
+          this.initScroll = 0
+        }
+        this.$refs.channels.style.transform = `translateX(-${50 * this.initScroll}%)`
+      },
+      rightList() {
+        this.initScroll += 1
+        if (this.initScroll > 19) {
+          this.initScroll = 19
+        }
+        this.$refs.channels.style.transform = `translateX(-${50 * this.initScroll}%)`
       }
     }
   }
 </script>
 
-<style scoped >
+<style scoped>
 
 </style>
